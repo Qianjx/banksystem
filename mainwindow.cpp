44cede539686fb8b40bd903792ae30f53a0306d2
Qianjx <qjx10@mail.ustc.edu.cn>
2018-06-07 13:02:35 +0800

    add

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,56 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    createConnection();
+
+    model=new QSqlTableModel(this);
+    ui->show_tableView->setModel(model);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+bool MainWindow::createConnection()
+{
+
+    db=QSqlDatabase::addDatabase("QMYSQL");
+    db.setHostName("localhost");
+    db.setDatabaseName("mybms");
+    db.setUserName("root");
+    db.setPassword("740832Qjx");
+    db.setPort(3306);
+
+    if(!db.open())
+    {
+        QMessageBox::critical(0,QObject::tr("error"),db.lastError().text());
+        return false;
+    }
+    return true;
+}
+//创建mysql连接
+
+void MainWindow::on_ClientQuerypushButton_clicked()
+{
+    model->setTable("book");
+    model->select();
+}
+
+void MainWindow::on_LoanquerypushButton_clicked()
+{
+    model->setTable("book");
+    model->select();
+}
+
+void MainWindow::on_AccountquerypushButton_clicked()
+{
+    model->setTable("book");
+    model->select();
+}
+
+void MainWindow::on_ClerkquerypushButton_clicked()
+{
+    model->setTable("book");
+    model->select();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,22 @@
 
 #include <QMainWindow>
 
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QtSql>
+#include <QSqlTableModel>
+#include <QSqlRelationalTableModel>
+//#include <QTableView>
+#include <QSqlError>
+#include <QString>
+#include <QLabel>
+#include <QTableWidget>
+#include <QPushButton>
+#include <QLineEdit>
+#include <QWidget>
+#include <QTextEdit>
+#include <QMessageBox>
+
 namespace Ui {
 class MainWindow;
 }
@@ -15,8 +31,22 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_ClientQuerypushButton_clicked();
+
+    void on_LoanquerypushButton_clicked();
+
+    void on_AccountquerypushButton_clicked();
+
+    void on_ClerkquerypushButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+
+    bool createConnection();//建立数据库连接
+    QSqlDatabase db;
+    QSqlQuery query;
+    QSqlTableModel *model;
 };
 
 #endif // MAINWINDOW_H
